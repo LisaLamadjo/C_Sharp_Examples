e531f59ad3ed622aee1cd1c88d59441806f58a69
Lisa <liza.shamarova.03@gmail.com>
2023-03-09 11:57:45 +0300

    1_2_hw

--- Lesson_01/HM_1_2/Program.cs
+++ Lesson_01/HM_1_2/Program.cs
@@ -1,1 +1,21 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих
+
+Console.WriteLine("Write the first nuber: ");
+
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Write the second nuber: ");
+
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Write the third nuber: ");
+
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.Write(max);
