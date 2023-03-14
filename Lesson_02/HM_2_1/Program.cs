3236d793a5eb84ba9197e1124e55f5ea1d86b706
Lisa <liza.shamarova.03@gmail.com>
2023-03-14 18:48:37 +0300

    HW_2_1

---
+++ Lesson_02/HM_2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// На вход принимает трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int num = int.Parse(Console.ReadLine()!);
+
+int result1 = num / 10 ;
+int result2 = result1 % 10;
+
+Console.WriteLine(result2);
