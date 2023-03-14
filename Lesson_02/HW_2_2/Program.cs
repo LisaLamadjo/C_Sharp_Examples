8a43b2cc66cb8d3237364baa379f4698a67ccd61
Lisa <liza.shamarova.03@gmail.com>
2023-03-14 19:06:05 +0300

    HW_2_2

---
+++ Lesson_02/HW_2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// выводит случайное трёхзначное число и удаляет вторую цифру числа
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+int PartNumber1 = num % 10;
+int PartNumber2 = num / 100;
+
+int a = PartNumber2 * 10;
+int result = a + PartNumber1;
+
+Console.WriteLine(result);
