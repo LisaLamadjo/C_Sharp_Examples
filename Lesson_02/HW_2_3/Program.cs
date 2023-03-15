a784c56572acdc0f164abaf962cf12a6ab0952e2
Lisa <liza.shamarova.03@gmail.com>
2023-03-15 14:03:59 +0300

    HW_2_3

--- Lesson_02/HW_2_3/Program.cs
+++ Lesson_02/HW_2_3/Program.cs
@@ -1,17 +1,16 @@
 ﻿// выводит третью цифру числа либо пишет что её нет
 
 int num = int.Parse(Console.ReadLine()!);
-
-if (num < 100)
+void ThirdNum(int num)
 {
-    Console.WriteLine("Нет третьей цифры");
-
-}
-else
-{
-    while (num > 999)
-    { num = num / 10;
-    return;} 
+    Console.Write($"{num} -> ");
+    if (num < 100)
+    {
+        Console.WriteLine("Нет третьей цифры");
+        return;
+    }
+    while (num > 999) num = num / 10;
+    Console.WriteLine(num % 10);
 }
 int result = num % 10;
 Console.WriteLine(result);
