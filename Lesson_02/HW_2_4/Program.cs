40546e9dd9b49902a004ca8e92e2485b7ec01c33
Lisa <liza.shamarova.03@gmail.com>
2023-03-15 14:23:49 +0300

    HW_2_4

--- Lesson_02/HW_2_4/Program.cs
+++ Lesson_02/HW_2_4/Program.cs
@@ -1,3 +1,13 @@
-﻿
-int num = int.Parse(Console.ReadLine()!);
-  Console.Write($"{num} -> ");
+﻿// принимает на вход цифру обозначающую день недели и проверяет является ли этот день выходным
+void Week(int num)
+{
+    string text = "not quite a week";
+
+    if (num == 6 || num == 7) text = "weekend";
+    else if ((num > 0) && (num < 6)) text = "workday";
+
+    Console.WriteLine($"{num} -> {text}");
+}
+
+int a = int.Parse(Console.ReadLine()!);
+Week(a);
