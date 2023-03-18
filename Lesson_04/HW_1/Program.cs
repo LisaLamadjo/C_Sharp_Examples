556811cba49f842d481865600f9b93b2828738e4
Lisa <liza.shamarova.03@gmail.com>
2023-03-18 13:07:17 +0300

    hw_4_1

---
+++ Lesson_04/HW_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите цикл, который принимает на вход
+//два числа (A и B) и возводит число A в натуральную степень B.
+
+int Stepen(int A, int B)
+{
+    int proiz = 1;
+    for (int count = 0; count < B; count++)
+    {
+        proiz *= A;  
+    
+    }
+    return proiz; 
+}
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+
+int result = Stepen(num_1, num_2);
+Console.WriteLine(result);
+
