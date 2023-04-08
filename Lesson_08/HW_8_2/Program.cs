3a31dc587a5af62f22e3c00b95b223ddb291ffd5
Lisa <liza.shamarova.03@gmail.com>
2023-04-08 11:03:01 +0300

    hw_8_2

--- Lesson_08/HW_8_2/Program.cs
+++ Lesson_08/HW_8_2/Program.cs
@@ -1,15 +1,7 @@
-﻿// программа находит строку с наименьшей суммой элементов в строке
+﻿// 2. Задайте прямоугольный двумерный массив. Напишите программу,
+//    которая будет находить строку с наименьшей суммой элементов.
 
-int[,] MassNums(int row, int column, int from, int to)
-{
-    int[,] arr = new int[row, column];
-    for (int i = 0; i < row; i++)
-        for (int j = 0; j < column; j++)
-            arr[i, j] = new Random().Next(from, to + 1);
-    return arr;
-}
 
-//выводящая функция
 void Print(int[,] arr)
 {
     int row_size = arr.GetLength(0);
@@ -18,34 +10,55 @@ void Print(int[,] arr)
     for (int i = 0; i < row_size; i++)
     {
         for (int j = 0; j < column_size; j++)
-            Console.Write($" {arr[i, j]} ");
+            Console.Write($" {arr[i, j],4} ");
         Console.WriteLine();
     }
     Console.WriteLine();
+
 }
 
-int Sum(int[,] arr)
+int[,] MassNums(int row, int column, int from, int to)
 {
-    int sum = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            sum += arr[i, j];
-        }
-        Console.WriteLine($"сумма чисел в {i + 1} строчки: {sum}");
-        sum = 0;
-    }
-    return sum;
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to + 1);
+
+    return arr;
 }
 
-//int MaxRow ()
+int[] SumNum(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    int[] all_nums = new int[row];
 
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            all_nums[i] += arr[i, j];
+        }
+    }
+    return all_nums;
+}
+
+void FindMin(int[] arr)
+{
+    int min_ind = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < arr[min_ind]) min_ind = i;
+    }
+    Console.WriteLine($"Sum of elements: {arr[min_ind]}, line: {min_ind + 1}");
+}
 
 Console.Write("Enter the number of rows: ");
 int row_num = int.Parse(Console.ReadLine()!);
 Console.Write("Enter the number of columns: ");
 int column_num = int.Parse(Console.ReadLine()!);
+
 Console.Write("Enter the min number of massive ");
 int start = int.Parse(Console.ReadLine()!);
 Console.Write("Enter the max number of massive ");
@@ -53,4 +66,6 @@ int stop = int.Parse(Console.ReadLine()!);
 
 int[,] mass = MassNums(row_num, column_num, start, stop);
 Print(mass);
-Sum(mass);
+
+int[] mass_sum = SumNum(mass);
+FindMin(mass_sum);
